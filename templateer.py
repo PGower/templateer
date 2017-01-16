671fd9c6fb3edd3e1de65ca59a162902ebd19810
Paul Gower <pgower@gmail.com>
2017-01-16 22:49:43 +1000

    Possibly not pretty but it does the job. Is able to use any available environment variables within the template + all Jinja2 template constructs. Will also allow for specifying the uid/gid of output files and the octal mode a file should be set to.

--- templateer.py
+++ templateer.py
@@ -7,9 +7,9 @@ import yaml
 @click.option('--config', '-c', type=click.File(mode='r'), default='./config.yml', help="Location of the config file. Defaults to ./config.yml")
 @click.option('--skip-empty/--no-skip-empty', help="When set any template that produces an all whitepace result will not be output to the destination.")
 # @click.option('--data-source', default='env', type=click.Choice(['env']), help="Reserved for future expansion. Currently has one possible value, environ.")
-def generate(config, skip_empty, data_source):
+def generate(config, skip_empty):
 	try:
-		config = yaml.loads(config)
+		config = yaml.load(config)
 	except (yaml.scanner.ScannerError, IOError) as e:
 		click.secho('An error occured and we were unable to load the config file at {}'.format(config.name), fg='red')
 		click.secho('The error message was: {}'.format(str(e)), fg='red')
@@ -18,25 +18,35 @@ def generate(config, skip_empty, data_source):
 		raise click.Abort('Invalid config file.')
 	else:
 		for template_id, template_config in config['templates'].items():
-			click.secho('Processing template with ID: {}'.format(template_id))
-			template = jinja2.Template(open(template_config['src']))
-			content = src.render(os.environ)
-			if skip_empty and content.strip():
-				click.secho('Not writing template with ID: {} as the output is empty.')
+			click.secho('Processing template with ID: {}'.format(template_id), fg='yellow')
+			template = jinja2.Template(open(template_config['src']).read())
+			content = template.render(os.environ)
+			if skip_empty and not content.strip():
+				click.secho('Not writing template with ID: {} as the output is empty.', fg='red')
 				continue
 			with open(template_config['dest'], 'w+') as f:
 				f.write(content)
 				click.secho('Writing content for template ID: {} to {}'.format(template_id, template_config['dest']))
 			uid = template_config.get('uid', -1)
 			gid = template_config.get('gid', -1)
-			os.chown(template_config['dest'], uid, gid)
-			click.secho('Setting uid and gid on destination file to {}:{}'.format(uid, gid))
+			try:
+				os.chown(template_config['dest'], uid, gid)
+				if uid != -1:
+					click.secho('Setting file uid to: {}'.format(uid))
+				if gid != -1:
+					click.secho('Setting file gid to: {}'.format(gid))
+			except OSError as e:
+				click.secho('Unable to chown file. Error is: {}'.format(str(e)), fg='red')
+			except TypeError as e:
+				click.secho('Invalid value for uid or gid given. Error is: {}'.format(str(e)))
 			if template_config.has_key('mode'):
-				os.lchmod(template_config['dest'], template_config['mode'])
-				click.secho('Setting file mode to {}'.format(template_config['mode']))
-
-
-
+				try:
+					os.chmod(template_config['dest'], template_config['mode'])
+					click.secho('Setting file mode to {}'.format(template_config['mode']))
+				except OSError as e:
+					click.secho('Unable to chmod file. Error is: {}'.format(str(e)), fg='red')
+				except TypeError as e:
+					click.secho('{} is not a valid value for chmod. Error is: {}'.format(template_config['mode'], str(e)))
 
 if __name__ == '__main__':
 	generate()
